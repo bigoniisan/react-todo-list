768e4099c918838041061be73c69d171577ef254
bigoniisan <johngod3@gmail.com>
2024-04-08 21:49:14 +1000

    added basic login form show on profile icon click

--- src/components/single/LoginButton/LoginButton.jsx
+++ src/components/single/LoginButton/LoginButton.jsx
@@ -1,24 +1,34 @@
+import { useRef, useState } from 'react';
 import Styles from './LoginButton.module.css';
 
 function LoginButton(props) {
 
+    const loginFormRef = useRef(null);
+    const [showLogin, setShowLogin] = useState(false);
+
+    function toggleLoginForm() {
+        setShowLogin(!showLogin);
+        showLogin ? loginFormRef.current.style.display = "flex" : loginFormRef.current.style.display = "none"; 
+    }
+
     return (
         <>
-        {/* TODO: make log in form display when account icon clicked */}
-            <form action="#" className={Styles.loginForm}>
+            {/* Pass Props From Child to Parent Component 
+                to call toggleLoginForm on LoginForm as a 
+                separate component */}
+            <a href="#">
+                {/* get profile icon from user */}
+                <div className={Styles.accountIcon} onClick={toggleLoginForm}>
+                
+                </div>
+            </a>
+            <form action="#" className={Styles.loginForm} ref={loginFormRef}>
                 <label htmlFor="username">Email</label>
                 <input type="text" name="email"/>
                 <label htmlFor="password">Password</label>
                 <input type="password" name="password"/>
                 <input type="submit" value="Sign in" />
             </form>
-            <a href="#">
-                {/* get profile icon from user */}
-                <div className={Styles.accountIcon}>
-                
-                </div>
-            </a>
-            
         </>
     )
 }
